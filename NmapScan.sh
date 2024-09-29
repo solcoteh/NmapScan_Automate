6b332e38e3b8488848b0a0d34c2e042dbbeeb2b3
solcoteh <121525098+solcoteh@users.noreply.github.com>
2024-09-30 01:51:45 +0330

    Update NmapScan.sh

--- NmapScan.sh
+++ NmapScan.sh
@@ -9,3 +9,11 @@ PORTS=$(nmap -p- $IP -T5 | grep -oE '[0-9]{1,5}/' | tr -d '\n' | tr '/' ',' | se
 # Run nmap on the extracted ports
 echo "Running nmap with the following ports: $PORTS"
 nmap -p $PORTS -sV $IP -T5
+
+# Check if ports 445 or 139 are in the extracted ports
+if [[ $PORTS == *"445"* || $PORTS == *"139"* ]]; then
+    echo "Ports 445 or 139 found! Running vulnerability scan..."
+    nmap -p 445,139 --script=vuln $IP -T5
+else
+    echo "Ports 445 or 139 not found, skipping vulnerability scan."
+fi
